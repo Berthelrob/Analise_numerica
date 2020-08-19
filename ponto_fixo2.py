5e868e85aaf957fe98d3081f60f483edc9d6d2a1
Robson Berthelsen <robson.brs@gmail.com>
2020-08-19 10:21:38 -0300

    Update ponto_fixo2.py

--- ponto_fixo2.py
+++ ponto_fixo2.py
@@ -4,22 +4,19 @@
 # 2. |g'(x)| < 1 para todo x\in[a,b]
 # f(x) = 0 <--> g(x) = x
 
-#def g(x):
-#    return (x + 7 / x) / 2
-def f(x):
-    #return x**3 + 8*x-7
-    return x**2-7
-
 def g(x):
-	#return (7-x**3)/8
-    return 1/2*(x+7/x)
+    return (x + (11 / x ** 2)) / 2
 
-n = 10
+def f(x):
+    return x ** 3 - 11
+
+n = 20
 a, b = [2, 3]
-x0 = 2
+x0 = 2.5
 for i in range(10):
     print('i:',i, '\t\traiz aproximada:', x0, ' \t\t\tf(x): ',f(x0))
-    print('i:',i, '\t\tPonto fixo: g(',x0,') = ',g(x0))
+    if(x0 == g(x0)):
+        print('\t\tPonto fixo: g(',x0,') = ',g(x0))
     xn = g(x0)
     x0 = xn
     
